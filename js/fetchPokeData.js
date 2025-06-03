da6504fd40405bdaa59e74f85e1031f3cb02410b
azen04 <anez2204@gmail.com>
2025-06-03 11:01:48 -0500

    final final final

--- js/fetchPokeData.js
+++ js/fetchPokeData.js
@@ -1,9 +1,18 @@
 
 function fetchPokemonData() {
     const promises = []
+    const registered = []
 
-    while (promises.length < 6) {
-        promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 151)}`))
+    while (promises.length < 7) {
+        const id = Math.floor(Math.random() * 151)
+
+        if (!registered.includes(id)) {
+            registered.push(id)
+        } else {
+            continue
+        }
+
+        promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${id}`))
     }
 
     return promises
